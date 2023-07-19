352fa033eaa13300fc5502903c929b182d8eb3e2
clenn <acrivopoulosdotcom@gmail.com>
2023-07-19 15:36:03 +0200

    Initial commit

---
+++ src/main/java/com/example/thirdweekbookapi/ThirdWeekBookapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdweekbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdWeekBookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdWeekBookapiApplication.class, args);
+	}
+
+}
